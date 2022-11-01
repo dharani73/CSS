993dd47a8692d725ec999075d1372c0d907bf84d
dharani73 <miriyala.dharani@gmail.com>
2022-11-01 15:03:55 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.4 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
